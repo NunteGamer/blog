95d1acebddd371706bc4b259a2a3ede26bc9a090
Nunte <97974456+NunteGamer@users.noreply.github.com>
2025-10-04 01:19:57 +0200

    Update quartz.config.ts

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "nuntemusic's blog",
+    pageTitle: "Nunte's Blog",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
@@ -23,7 +23,7 @@ const config: QuartzConfig = {
       fontOrigin: "googleFonts",
       cdnCaching: true,
       typography: {
-        header: "Schibsted Grotesk",
+        header: "Oswald Bold",
         body: "Nunito",
         code: "Oswald Bold",
       },
