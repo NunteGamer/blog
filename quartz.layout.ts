af11c8a79186b74a0b9dbe0aae348521e794a3a3
Nunte <97974456+NunteGamer@users.noreply.github.com>
2025-10-04 01:14:23 +0200

    Update quartz.layout.ts

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -5,7 +5,23 @@ import * as Component from "./quartz/components"
 export const sharedPageComponents: SharedLayout = {
   head: Component.Head(),
   header: [],
-  afterBody: [],
+  afterBody: [
+  Component.Comments({
+    provider: 'giscus',
+    options: {
+      // from data-repo
+      repo: 'NunteGamer/blog',
+      // from data-repo-id
+      repoId: 'R_kgDOP6jo3w',
+      // from data-category
+      category: 'Announcements',
+      // from data-category-id
+      categoryId: 'DIC_kwDOP6jo384CwN9p',
+      // from data-lang
+      lang: 'es'
+    }
+  }),
+],
   footer: Component.Footer({
     links: {
       GitHub: "https://github.com/jackyzha0/quartz",
